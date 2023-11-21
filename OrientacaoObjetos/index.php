c855134274b4e71c9c4261a0bb39662580e2cfb7
pedro <pedro.prccau@gmail.com>
2023-11-21 11:53:25 -0300

    vai tomar no cu

---
+++ OrientacaoObjetos/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+    <form action="" method="post">
+
+        <div class="main">
+            <div class="nameDiv">
+                <h3 class="title" >Nome:</h3>
+                <input type="text" class="name" name="name" id="name" placeholder="Nome do usuário">
+            </div>
+            <div class="submit">
+                <button type="submit" class="submit-button">Submit</button>
+            </div>
+
+
+        </div>
+    </form>
+
+
+</body>
+</html>
